9023c46b2a2d4f5b048349fbceb96f904b8f92a0
himekatie <useforanything9@gmail.com>
2024-09-24 16:59:01 -0400

    Added Reset button and header styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./components/Header/Header"
+import './assets/fonts.css'
 function App() {
   return (
     <div className="App">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     return(
         <header className='header'>
             <h1>tic-tac-toe</h1>
+            <button><h2>reset</h2></button>
         </header>
     );
     
