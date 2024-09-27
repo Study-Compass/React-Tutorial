be92984fc037f3ad2fcb962bfab6c4e826fbfe4a
himekatie <useforanything9@gmail.com>
2024-09-27 19:14:41 -0400

    Made the Player part above the board, and made it switch bewtween players. Also made onclick function for X's and 0's for player 1 and player 2.

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -4,6 +4,7 @@ import Tile from "./Tile/Tile";
 function Board(){
 
     const [board, setBoard] = useState([[null, null, null],[null, null, null],[null, null, null]])
+    const [player,setPlayer] = useState(0);
 
     const changeBoard = (row, col, value) => {
         //changes board[row][col] to value
@@ -13,19 +14,22 @@ function Board(){
         const copy = [...board];
         copy[row][col] = value;
         setBoard(copy);
-
-
+        setPlayer(player === 0 ? 1 : 0);
     }
+    //If value = null, then empty
+    //If value = 0, then 0
+    //If value = 1, then x
 
     return(
         <div className="board">
+            <div className={`player ${player === 0 ? "" : "player2"}`}><h1>player {player+1}'s turn</h1></div>
             {
                 board.map((row, index)=>{
                     return(
                         <div className="board-row" key={`row${index}`}>
                             {
                                 row.map((tile, colIndex)=>{
-                                    return <Tile row={index} col={colIndex}/>
+                                    return <Tile row={index} col={colIndex} value={board[index][colIndex]} changeBoard={changeBoard} player={player}/>
                                 })
                             }
                         </div>
--- src/components/Board/Tile/Tile.jsx
+++ src/components/Board/Tile/Tile.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Tile.css";
+//import Logo from '../../assets/icons/cross.svg';
 
-function Tile({row, col}){
+function Tile({row, col, value, changeBoard, player}){
+    function handleClick(){
+        if (value === null){
+            if (player === 0){
+                changeBoard(row,col,0);
+            }
+            else if (player === 1){
+                changeBoard(row,col,1);
+            }
+        }
+
+    }
     return(
-        <div className="tile">
-
+        <div className="tile" onClick={handleClick}>
+            {value && value === 1 ? "X" : value === 0 ? "0" : ''}
         </div>
     )
 }
